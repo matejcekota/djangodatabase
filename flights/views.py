0b45b16cb5c48b6e23725889616492a90bbffdfe
matěj cekota <d21770@oaopava.cz>
2024-02-20 10:36:56 +0100

    push

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,14 @@
+from django.shortcuts import render
+
 from .models import Flight
 
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+          "flight": flight
     })
